0e5ec705b343111fa4c6c02d0fb9d03e0ef15193
Smundrigi <32314174+Smundrigi@users.noreply.github.com>
2019-01-08 19:41:24 -0600

    Create appConstants.java
    Added Constants file

---
+++ mobile-app/src/constants/appConstants.java
@@ -0,0 +1,11 @@
+package constants;
+
+public class appConstants 
+{
+	
+	public static String DEVICENAME = "276e06d67ce5";
+	public static String DEVICEID = "276e06d67ce5";
+	
+	public static String IPADD = "276e06d67ce5";
+	public static String PLATFORM = "Android";
+}
